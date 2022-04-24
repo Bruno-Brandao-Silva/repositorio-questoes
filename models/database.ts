5b92a6a1dd18e7a954ffc81393f6375655eeea99
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2022-04-24 12:58:09 -0300

    model/database.ts - 100%

--- models/database.ts
+++ models/database.ts
@@ -7,24 +7,96 @@ const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 const db = client.db('test')
 
 export default new class database {
+
     constructor() {
 
     }
 
-    async collect() {
+    async insertOne(collection: string, array: {}) {
         try {
             await client.connect()
-            const collection = await db.collection('devices').find().toArray()
-            return collection
+            const result = await db.collection(collection).insertOne(array);
+            return result;
         } finally {
             await client.close();
-
         }
     }
-    async insert(array:any) {
+
+    async insertMany(collection: string, array: [], ordered = true) {
         try {
             await client.connect()
-            await db.collection('devices').insertOne(array);
+            const result = await db.collection(collection).insertMany(array, { ordered: ordered });
+            return result;
+        } finally {
+            await client.close();
+        }
+    }
+
+    async findOne(collection: string, query = {}, options = {}) {
+        try {
+            await client.connect()
+            const result = await db.collection(collection).findOne(query, options)
+            return result
+        } finally {
+            await client.close();
+        }
+    }
+
+    async findAll(collection: string, query = {}, options = {}) {
+        try {
+            await client.connect()
+            const result = await db.collection(collection).find(query, options).toArray()
+            return result
+        } finally {
+            await client.close();
+        }
+    }
+
+    async updateOne(collection: string, query = {}, update: {}, options = {}) {
+        try {
+            await client.connect()
+            const result = await db.collection(collection).updateOne(query, update, options)
+            return result
+        } finally {
+            await client.close();
+        }
+    }
+
+    async updateMany(collection: string, query = {}, update: {}, options = {}) {
+        try {
+            await client.connect()
+            const result = await db.collection(collection).updateMany(query, update, options)
+            return result
+        } finally {
+            await client.close();
+        }
+    }
+
+    async replaceOne(collection: string, query = {}, replacement: {}, options = {}) {
+        try {
+            await client.connect()
+            const result = await db.collection(collection).replaceOne(query, replacement, options)
+            return result
+        } finally {
+            await client.close();
+        }
+    }
+
+    async deleteOne(collection: string, query = {}, options = {}) {
+        try {
+            await client.connect()
+            const result = await db.collection(collection).deleteOne(query, options)
+            return result
+        } finally {
+            await client.close();
+        }
+    }
+
+    async deleteMany(collection: string, query = {}, options = {}) {
+        try {
+            await client.connect()
+            const result = await db.collection(collection).deleteMany(query, options)
+            return result
         } finally {
             await client.close();
         }
