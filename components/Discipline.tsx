d500be4f6322caad761cd226eeb8da2f72a9b0ee
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2022-05-28 12:24:13 -0300

    Vizualização de multiplas imagens

--- components/Discipline.tsx
+++ components/Discipline.tsx
@@ -1,13 +1,46 @@
 import Link from 'next/link'
+import { useState } from 'react'
+import styles from '../styles/DisciplineCad.module.css'
 
-export default function DisciplineComponent(discipline: any) {
+type Props = {
+    name: string
+    description: string
+    length: string
+    image: string[]
+}
+export default function DisciplineComponent(props: Props) {
+    const [imageIndex, setImageIndex] = useState(0)
+    const PreviousImageButton = () => {
+        if (imageIndex > 0) {
+            setImageIndex(imageIndex - 1)
+        }
+    }
+    const NextImageButton = () => {
+        if (imageIndex < props.image.length - 1) {
+            setImageIndex(imageIndex + 1)
+        }
+    }
     return (
-        <li>
-            <b>
-                <Link href="/discipline/[id]" as={`/discipline/${discipline.discipline._id}`}>
-                    <a>{discipline.discipline.name}</a>
-                </Link>
-            </b>
-        </li>
+        <div className={styles.ContainerPreview}>
+            <fieldset className={styles.FieldsetPreview}>
+                <div className={styles.ImageControlBox}>
+                    <div className={styles.PreviousImageButton} onClick={PreviousImageButton}>
+                        <div className={styles.Bar1}></div>
+                        <div className={styles.Bar2}></div>
+                    </div>
+                    <div className={styles.ImageContainerPreview}>
+                        <img className={styles.ImagePreview} src={props?.image?.[imageIndex] || 'discipline-placeholder.png'}></img>
+                    </div>
+                    <div className={styles.NextImageButton} onClick={NextImageButton}>
+                        <div className={styles.Bar1}></div>
+                        <div className={styles.Bar2}></div>
+                    </div>
+                </div>
+                <label className={styles.LabelName}>{props.name || 'Disciplina'}</label>
+                <label className={styles.LabelDescription}>{props.description || 'Descrição'}</label>
+                <label className={styles.LabelLength}>{props.length || 'Tamanho'}</label>
+            </fieldset>
+
+        </div>
     )
 }
--- pages/discipline/index.tsx
+++ pages/discipline/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import { useState } from 'react'
 import useSWR from 'swr'
 import DefaultHead from '../../components/DefaultHead'
+import DisciplineComponent from '../../components/Discipline'
 import Header from '../../components/Header'
 import InfinityLoading from '../../components/InfinityLoading'
 import styles from '../../styles/DisciplineCad.module.css'
@@ -14,11 +15,78 @@ export default function Create() {
     const [length, setLength] = useState('')
     const [imageInput, setImageInput] = useState("")
     const [imageFiles, setImageFiles] = useState<FileList>()
-    const [image, setImage] = useState('')
+    const [image, setImage] = useState<string[]>([])
+
+    const handleImageInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setImageInput(e.target.value)
+        if (e.target.files) {
+            setImageFiles(e.target.files)
+            if (e.target.files.length > 0) {
+                const temp = []
+                for (let i = 0; i < e.target.files.length; i++) {
+                    temp.push(URL.createObjectURL(e.target.files[i]))
+                }
+                setImage(temp);
+            } else {
+                setImage(['discipline-placeholder.png'])
+            }
+        }
+    }
+
+    const handleCreate = async (e: any) => {
+        try {
+            var formData = new FormData()
+            for (let i = 0; imageFiles && i < imageFiles.length; i++) {
+                formData.append('file', imageFiles[i])
+            }
+            setStatus(true)
+            fetch('api/image/upload', {
+                method: "POST",
+                body: formData,
+            }).then(res => res.json())
+                .then(res => {
+                    if (res.files) setImage(res.files[0].host + res.files[0].filename)
+                    console.log(res.files)
+                    const data: any = { name, description, length }
+                    const formBody = [];
+                    for (var property in data) {
+                        var encodedKey = encodeURIComponent(property);
+                        var encodedValue = encodeURIComponent(data[property]);
+                        formBody.push(encodedKey + "=" + encodedValue);
+                    }
+                    for (let i = 0; res.files && i < res.files.length; i++) {
+                        var encodedKey = encodeURIComponent('imageFilesName');
+                        var encodedValue = encodeURIComponent(res.files[i].filename);
+                        formBody.push(encodedKey + "=" + encodedValue);
+                    }
+                    const encodedBody = formBody.join("&");
+
+                    fetch('api/discipline/', {
+                        method: "POST",
+                        redirect: 'follow',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                        },
+                        body: encodedBody,
+                    }).then(res => {
+                        if (!res.url) window.location.href = res.url
+                    }).catch(error => {
+                        console.log(error)
+                        setStatus(false)
+                    });
+                }).catch(error => {
+                    console.log(error)
+                    setStatus(false)
+                }).finally(() => setStatus(false));
+        } catch (err) {
+            console.log(err);
+            setStatus(false)
+
+        }
+    }
     return (
         <>
             <DefaultHead />
-
             <Header />
             <InfinityLoading active={status} />
             <section className={styles.Section}>
@@ -40,83 +108,13 @@ export default function Create() {
                         </div>
                         <div className={styles.InputContainer}>
                             <label>Imagem da disciplina</label>
-                            <input className={styles.InputImage} name='image' type='file' accept='image/*' multiple value={imageInput} onChange={e => {
-                                setImageInput(e.target.value)
-                                if (e.target.files) {
-                                    setImageFiles(e.target.files)
-                                    if (e.target.files.length > 0) {
-                                        setImage(URL.createObjectURL(e.target.files[0]));
-                                    } else {
-                                        setImage('discipline-placeholder.png')
-                                    }
-                                }
-                            }}></input>
+                            <input className={styles.InputImage} name='image' type='file' accept='image/*' multiple value={imageInput} onChange={e => handleImageInput(e)}></input>
                         </div>
                         <div >
-                            <button type="button" onClick={async () => {
-                                try {
-                                    var formData = new FormData()
-                                    for (let i = 0; imageFiles && i < imageFiles.length; i++) {
-                                        formData.append('file', imageFiles[i])
-                                    }
-                                    setStatus(true)
-                                    fetch('api/image/upload', {
-                                        method: "POST",
-                                        body: formData,
-                                    }).then(res => res.json())
-                                        .then(res => {
-                                            if (res.files) setImage(res.files[0].host + res.files[0].filename)
-                                            console.log(res.files)
-                                            const data: any = { name, description, length }
-                                            const formBody = [];
-                                            for (var property in data) {
-                                                var encodedKey = encodeURIComponent(property);
-                                                var encodedValue = encodeURIComponent(data[property]);
-                                                formBody.push(encodedKey + "=" + encodedValue);
-                                            }
-                                            for (let i = 0; res.files && i < res.files.length; i++) {
-                                                var encodedKey = encodeURIComponent('imageFilesName');
-                                                var encodedValue = encodeURIComponent(res.files[i].filename);
-                                                formBody.push(encodedKey + "=" + encodedValue);
-                                            }
-                                            const encodedBody = formBody.join("&");
-
-                                            fetch('api/discipline/', {
-                                                method: "POST",
-                                                redirect: 'follow',
-                                                headers: {
-                                                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-                                                },
-                                                body: encodedBody,
-                                            }).then(res => {
-                                                if (!res.url) window.location.href = res.url
-                                            }).catch(error => {
-                                                console.log(error)
-                                                setStatus(false)
-                                            });
-                                        }).catch(error => {
-                                            console.log(error)
-                                            setStatus(false)
-                                        }).finally(() => setStatus(false));
-                                } catch (err) {
-                                    console.log(err);
-                                    setStatus(false)
-
-                                }
-                            }}>Criar</button>
+                            <button type="button" onClick={e => handleCreate(e)}>Criar</button>
                         </div>
                     </form>
-                    <div className={styles.ContainerPreview}>
-                        <fieldset className={styles.FieldsetPreview}>
-                            <div className={styles.ImageContainerPreview}>
-                                <img className={styles.ImagePreview} src={image || 'discipline-placeholder.png'}></img>
-                            </div>
-                            <label className={styles.LabelName}>{name || 'Disciplina'}</label>
-                            <label className={styles.LabelDescription}>{description || 'Descrição'}</label>
-                            <label className={styles.LabelLength}>{length || 'Tamanho'}</label>
-                        </fieldset>
-
-                    </div>
+                    <DisciplineComponent name={name} description={description} length={length} image={image} />
                 </div >
             </section>
         </>
