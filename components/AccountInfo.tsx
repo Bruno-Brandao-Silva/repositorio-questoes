01dd431b419e4dad80ce0f40ebf9e4e02b489c62
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2022-06-09 22:39:32 -0300

    a -> Link

--- components/AccountInfo.tsx
+++ components/AccountInfo.tsx
@@ -15,7 +15,7 @@ const AccountInfo: NextComponentType = () => {
                     <div className={styles.AccountBtn}>
                         <Link href='/perfil'><a className={styles.RegisterBtn}>Perfil</a></Link>
                         <div className={styles.SeparatorBar} >|</div>
-                        <a className={styles.LoginBtn} href="/api/auth/logout">Sair</a>
+                        <Link href="/api/auth/logout"><a className={styles.LoginBtn}>Sair</a></Link>
                     </div>
                     <div style={{ position: "fixed", bottom: 0, right: 15 + 'px' }}>
 
@@ -32,7 +32,7 @@ const AccountInfo: NextComponentType = () => {
                     <img className={styles.ProfileImg} alt={'Profile'} src="/profile-placeholder.png" onClick={() => { }}></img>
                 </div>
                 <div className={styles.AccountBtn}>
-                    <a href="/api/auth/login" className={styles.LoginBtn} onClick={() => { }}>Entrar</a>
+                    <Link href="/api/auth/login"><a className={styles.LoginBtn}>Entrar</a></Link>
                 </div>
 
             </div >
